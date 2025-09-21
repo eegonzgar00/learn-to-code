8291f3733b6f0b55535dcb6e79945ae63893a4f6
Ed <egonzgar00@gmail.com>
2025-09-21 17:39:46 +0200

    se afgrega proyecto base de angular

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { LoginComponent } from './app/login/login.component';
+import { DashboardComponent } from './app/dashboard/dashboard.component';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: 'login', component: LoginComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ]),
+  ],
+}).catch(err => console.error(err));
